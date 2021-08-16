ef4cd0d8cee53246c3668a170f3f1c9cbf99627e
felipelima94 <fe.max@hotmail.com>
2021-08-16 15:01:09 -0300

    Initial commit

---
+++ Animacao_3/Criptografia.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace Animacao_3
+{
+    class Criptografia
+    {
+        /*
+         * Variaveis privadas para serem utilizada para gerar a chave de criptografia
+         */
+        private static DateTime Data = DateTime.Now; // chama a class DateTime (Data Atual)
+        private static string day = twoNum(Data.Day); // pega o dia
+        private static string month = twoNum(Data.Month); // pega o mes
+        private static string year = Convert.ToString(Data.Year); // pega o ano
+
+        private static string secretKey = day + month + year + "60868"; // cria a chave da criptografia
+
+        private static string twoNum(int num)
+        {
+            //metodo para verificar se são dois numeros
+            string newNum = Convert.ToString(num);
+            if (num <= 9)
+            {
+                newNum = "0" + num;
+            }
+            return newNum;
+        }
+        
+        private static string Encrypt(string toEncrypt)
+        {
+            /*
+             * Metodo para criptografar as informação
+             * informação sendo a senha (Data atual + ultimos digitos do RA)
+            */
+            byte[] keyArray;
+            byte[] toEncryptArray = UTF8Encoding.UTF8.GetBytes(toEncrypt);
+
+            string key = "60868"; // chave para criptografar a informação
+            
+            //metodo de criptografia em 
+            // ele criptografa a chave que será usado na criptografia
+            MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider();
+            keyArray = hashmd5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
+            // Sempre libera recursos e libera dados do serviço da criptografia fornecido.
+
+            hashmd5.Clear();
+
+            TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+            // define uma chave(key) secreta para o TripleDES
+            tdes.Key = keyArray; // define a hash, a chave em hexadecimal que será usada para a criptografia
+            
+            // nos escolhemes o EBC(Eletronic code book)
+            tdes.Mode = CipherMode.ECB; // modo operação simetrica que será usada para a criptografia
+
+            // modo de preenchimento(se qualquer byte extra precisa ser adicionado)
+            tdes.Padding = PaddingMode.PKCS7;
+
+            /* -------------------------------------------------------------------------------- */
+
+            // cria a criptografia baseada na TDES Triple Data Encryption Standard (Padrão de criptografia de dados triplo);
+            ICryptoTransform cTransform = tdes.CreateEncryptor();
+
+            // trsnaforma as regioes especifiadas em bytes para array e guarda na variavel "resultArray"
+            byte[] resultArray =
+                // transforma a informação em bytes
+                cTransform.TransformFinalBlock(
+                    toEncryptArray, // informação a ser criptografada
+                    0, // inicio da contagem de bytes
+                    toEncryptArray.Length // limite da contagem
+                );
+
+            // Liberar recursos mantidos por tripleDES criptografador
+            tdes.Clear();
+
+            //return the encrypted data into unreadable string format
+            // retorna o informação em formato string que não pode ser lido
+            return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+        }
+
+        private static string Decrypt(string cipherString)
+        {
+            /*
+             * Metodo para descriptografar
+             */
+            byte[] keyArray;
+            byte[] toEncryptArray = Convert.FromBase64String(cipherString);
+
+            string key = "60868"; // chave para criptografar a informação
+            
+            //metodo de criptografia em 
+            // ele criptografa a chave que será usado na criptografia
+            MD5CryptoServiceProvider hashmd5 = new MD5CryptoServiceProvider();
+            keyArray = hashmd5.ComputeHash(UTF8Encoding.UTF8.GetBytes(key));
+            // Sempre libera recursos e libera dados do serviço da criptografia fornecido.
+
+            hashmd5.Clear();
+            
+            TripleDESCryptoServiceProvider tdes = new TripleDESCryptoServiceProvider();
+            // define uma chave(key) secreta para o TripleDES
+            tdes.Key = keyArray; // define a hash, a chave em hexadecimal que será usada para a criptografia
+            
+            // nos escolhemes o EBC(Eletronic code book)
+            tdes.Mode = CipherMode.ECB; // modo operação simetrica que será usada para a criptografia
+
+            // modo de preenchimento(se qualquer byte extra precisa ser adicionado)
+            tdes.Padding = PaddingMode.PKCS7;
+
+            /* -------------------------------------------------------------------------------- */
+
+            // cria a criptografia baseada na TDES Triple Data Encryption Standard (Padrão de criptografia de dados triplo);
+            ICryptoTransform cTransform = tdes.CreateDecryptor();
+
+            // trsnaforma as regioes especifiadas em bytes para array e guarda na variavel "resultArray"
+            byte[] resultArray =
+                // transforma a informação em bytes
+                cTransform.TransformFinalBlock(
+                    toEncryptArray, // informação a ser criptografada
+                    0, // inicio da contagem de bytes
+                    toEncryptArray.Length // limite da contagem
+                );
+
+            // Liberar recursos mantidos por tripleDES criptografador
+            tdes.Clear();
+
+            //return the Clear decrypted TEXT
+            return UTF8Encoding.UTF8.GetString(resultArray);
+        }
+
+
+        // variaveis publicas para serem usadas para comparação
+        public static string Encrypted = Encrypt(secretKey);
+        // metodo para verificação da key digitada
+        public static bool DesCrypeted (string senhaInfo)
+        {
+            if (senhaInfo == Decrypt(Encrypted))
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Animacao_3/MainWindow.xaml.cs
@@ -0,0 +1,192 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
+
+namespace Animacao_3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static Storyboard StoryB = new Storyboard();
+        static int estagio = 0;
+        static int tentativa = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            code.Content = code.Content + Criptografia.Encrypted;
+        }
+        
+        private void estagios_ligar(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if ( estagio == 0 )
+            {
+                StoryB = (Storyboard)TryFindResource("Estagio_I");
+                StoryB.Begin();
+                estagio = 1;
+
+                // aperte os sintos
+                StoryB = (Storyboard)TryFindResource("AperteoCinto");
+                StoryB.RepeatBehavior = RepeatBehavior.Forever;
+                StoryB.Begin();
+
+                StoryB = (Storyboard)TryFindResource("ligar");
+                StoryB.Begin();
+
+                // libera para digitar a senha
+                pass.IsEnabled = true;
+                pass.Focus();
+            }
+            else if (estagio == 1)
+            {
+                // partida do carro
+                StoryB = (Storyboard)TryFindResource("Estagio_II");
+                StoryB.Begin();
+                estagio = 2;
+
+                Partida();
+            } else
+            {
+                // desligar tudo
+                Desligar();
+                StoryB = (Storyboard)TryFindResource("AperteoCinto");
+                StoryB.Stop();
+                StoryB = (Storyboard)TryFindResource("ligar");
+                StoryB.Stop();
+                // desabilita para digitar a senha
+                pass.IsEnabled = false;
+            }
+        }
+
+        private void bntLigar(object sender, MouseButtonEventArgs e)
+        { // metodo para ligar ou desligar o carro sem desligar o painel e desativar tudo
+            if (estagio == 0) {
+                estagios_ligar(null, null);
+            } else if(estagio == 2)
+            {
+                estagio = 1;
+                MsgCarOn(false, false);
+
+                StoryB = (Storyboard)TryFindResource("Estagio_I");
+                StoryB.Begin();
+                StoryB = (Storyboard)TryFindResource("Desacelerar");
+                StoryB.Begin();
+
+                // libera para digitar a senha
+                pass.IsEnabled = true;
+                pass.Focus();
+            } else if (estagio == 1)
+            {
+                estagios_ligar(null, null);
+            }
+        }
+
+        private void bntDesligar(object sender, MouseButtonEventArgs e)
+        { // metodo para desligar o carro independente do estagio da chave que estiver
+            if (tentativa >= 3)
+            {
+                tentativa = 0;
+            }
+            estagio = 2;
+            estagios_ligar(null, null);
+            
+        }
+
+        private void check(object sender, KeyEventArgs e)
+        {   // evento ao clicar em enter faz a verificação e da partida
+            if( e.Key == Key.Enter)
+            {
+                Partida();
+                estagios_ligar(null, null);
+            }
+        }
+
+        private void Partida()
+        {
+            // da partida no carro caso a senha esteje correta
+            if (Criptografia.DesCrypeted(pass.Text))
+            {
+                StoryB = (Storyboard)TryFindResource("partida");
+                StoryB.Begin();
+
+                MsgCarOn(true, true);
+
+                pass.IsEnabled = false;
+            } else
+            {
+                MsgCarOn(true, false);
+
+                StoryB = (Storyboard)TryFindResource("Estagio_I");
+                StoryB.Begin();
+                estagio = 1;
+            }
+        }
+
+        private void Desligar()
+        {
+            // desliga o carro 
+            // esconde a mensagem de que o carro está ligado
+            // para a aceleração
+            MsgCarOn(false, false);
+
+            StoryB = (Storyboard)TryFindResource("DesligarMotor");
+            StoryB.Begin();
+
+            StoryB = (Storyboard)TryFindResource("Estagio_0");
+            StoryB.Begin();
+            estagio = 0;
+
+            pass.Text = "";
+        }
+
+        private void MsgCarOn(bool on, bool senha)
+        {
+            // mostra a mensagem se o carro estiver ligado ou desligado.
+            if (on)
+            {
+                if (senha)
+                {
+                    StoryB = (Storyboard)TryFindResource("Ligado");
+                    ligado.Content = "A senha está correta e o carro foi ligado!";
+                    tentativa = 0;
+                } else
+                {
+                    StoryB = (Storyboard)TryFindResource("Erro");
+                    ligado.Content = "Senha Incorreta!";
+                    tentativa++;
+                }
+
+                if (tentativa >= 3)
+                {
+                    StoryB = (Storyboard)TryFindResource("Alarme");
+                    ligado.Content = "O alarme foi ligado!";
+                    pass.IsEnabled = false;
+                }
+
+                ligado.Visibility = Visibility;
+
+                StoryB.RepeatBehavior = RepeatBehavior.Forever;
+                StoryB.Begin();
+            } else
+            {
+                StoryB.Stop();
+                ligado.Visibility = Visibility.Hidden;
+            }
+        }
+        
+
+        private void OpemAbout(object sender, RoutedEventArgs e)
+        {   // Abre a janela sobre o programa
+            About sobre = new About();
+            sobre.Show();
+        }
+
+        private void AbrirAjuda(object sender, RoutedEventArgs e)
+        {   // abre a janela ajuda
+            Ajuda ajuda = new Ajuda();
+            ajuda.Show();
+        }
+    }
+}
